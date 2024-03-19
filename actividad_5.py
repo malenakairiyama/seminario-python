052f2fe96d115b20953d519d3dcc04971aacd6b5
malenakairiyama <malenakairiyamas@gmail.com>
2024-03-18 22:04:00 -0300

    act 5 done

---
+++ actividad_5.py
@@ -0,0 +1,16 @@
+#Implementa un programa que solicite al usuario que ingrese una lista
+#de números. Luego, imprime la lista pero detén la impresión si 
+# encuentras un número negativo. Nota: utilice la sentencia break 
+# cuando haga falta.
+
+# Solicitar al usuario que ingrese los números separados por comas
+entrada = input("Ingresa los números separados por comas: ")
+
+# Dividir la entrada en una lista de números
+numeros_texto = entrada.split(',')
+
+# Convertir cada número de texto a un número entero
+numeros = [int(numero) for numero in numeros_texto]
+
+# Imprimir la lista de números ingresados
+print("Lista de números ingresados:", numeros)
